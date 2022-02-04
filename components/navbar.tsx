24f1f11868673eff53a69da62d568e004e31612b
jmossesgeld <jmossesgeld@gmail.com>
2022-02-04 09:09:43 +0800

    create title

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,29 +4,24 @@ import Image from "next/image";
 
 export default function NavBar() {
   return (
-    <nav className={styles.navbar}>
-      <Image
-        src="/images/profile2.jpg"
-        height={50}
-        width={50}
-        alt="profile img"
-        className={styles.borderCircle}
-      />
-
-      <ul>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#">Skills</a>
-        </li>
-        <li>
-          <a href="#">Projects</a>
-        </li>
-        <li style={{ border: 0 }}>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
+    <nav className={styles.navbar} id="#navbar">
+      <div className={styles.img}>
+        <Image
+          src="/images/profile2.jpg"
+          height={50}
+          width={50}
+          alt="profile img"
+          className={styles.borderCircle}
+        />
+      </div>
+      <div className={styles.items}>
+        <a href="#about">About</a>
+        <a href="#">Skills</a>
+        <a href="#">Projects</a>
+        <a href="#" style={{ border: 0 }}>
+          Contact
+        </a>
+      </div>
     </nav>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,57 @@ import type { NextPage } from "next";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import NavBar from "../components/navbar";
+import { useState, useEffect } from "react";
 
 const Home: NextPage = () => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    setVisible(true);
+  }, []);
+
   return (
     <>
       <NavBar />
-      <div className={styles.container}>
-        <div className={styles.title}>
+
+      {/* Homepage */}
+      <div className={styles.home}>
+        <div className={`${styles.title} ${visible && styles.visible}`}>
           <h1>Hi! </h1>
           <h1>I am John Al Mossesgeld</h1>
           <h5>SOFTWARE ENGINEER</h5>
         </div>
-        <div className={styles.img}>
-          <Image src="/images/profile.jpg" height={144} width={144} alt="profile img" />
+      </div>
+
+      {/* About */}
+      <div className={styles.about} id="about">
+        <div className={styles.aboutTitle}>
+          <h2>About Me</h2>
+        </div>
+        <div className={styles.aboutContent}>
+          <div className={styles.aboutContentLeft}>
+            <div className={styles.aboutContentLeftImg}>
+              <Image
+                src="/images/profile.jpg"
+                height={200}
+                width={200}
+                alt="profile img"
+                className={styles.borderCircle}
+              />
+            </div>
+            <div className={styles.aboutContentLeftText}>
+              <p>
+                I am a software engineer with a passion for building software that improves the
+                lives of others. I have a background in computer science and mathematics, and I am
+                currently pursuing a B.S. in Computer Science at the University of Waterloo.
+              </p>
+              <p>
+                I am currently looking for a full-time position as a software engineer. If you are
+                interested in working with me, please contact me at{" "}
+                <a href="mailto:jmossesgeld@gmail.com"></a>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
