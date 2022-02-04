191a481cc4a1d9d04135521f31ba70811942cf0b
jmossesgeld <jmossesgeld@gmail.com>
2022-02-04 18:26:36 +0800

    add title

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,9 +1,13 @@
 import BgVideo from "./bgVideo";
 import NavBar from "./navbar";
+import Head from "next/head";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
+        <Head>
+            <title>John Al Mossesgeld</title>
+        </Head>
       <NavBar />
       <BgVideo />
       {children}
